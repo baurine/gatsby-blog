fe81f6adae46c8d4c683fe6a731ea29834a26c17
baurine <2008.hbl@gmail.com>
2018-12-02 13:49:19 +0800

    Install gatsby-source-filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
