7cb649f59ad3a0892e05224e44cff1baf6990eb7
baurine <2008.hbl@gmail.com>
2018-12-05 11:20:39 +0800

    Use `gatsby-remark-copy-linked-files` plugin to handle related local files in markdown document

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,38 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
-    `gatsby-transformer-remark`,
+    // `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // "gatsby-remark-copy-linked-files",
+          {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              // `ignoreFileExtensions` defaults to [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
+              // as we assume you'll use gatsby-remark-images to handle
+              // images in markdown as it automatically creates responsive
+              // versions of images.
+              //
+              // If you'd like to not use gatsby-remark-images and just copy your
+              // original images to the public directory, set
+              // `ignoreFileExtensions` to an empty array.
+              ignoreFileExtensions: [],
+            }
+          },
+          // {
+          //   resolve: `gatsby-remark-images`,
+          //   options: {
+          //     // It's important to specify the maxWidth (in pixels) of
+          //     // the content container as this plugin uses this as the
+          //     // base for generating different widths of each image.
+          //     maxWidth: 590,
+          //   },
+          // },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
